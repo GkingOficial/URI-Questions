3cef7093325e94c4ec8783a12714840813f1d24f
GkingOficial <59315570+GkingOficial@users.noreply.github.com>
2020-01-24 18:48:34 -0300

    Ordenando a Lista de Crianças  do Papai Noel.

--- 2479.c
+++ 2479.c
@@ -8,10 +8,9 @@ typedef struct{
 void Order(kids arr[], int size);
 
 int main (){
-	int i, good_behavior = 0, bad_behavior = 0, amount, aux;
+	int i = 0, good_behavior = 0, bad_behavior = 0, amount, aux;
 	char behavior;
 	
-	i = 0;
 	scanf("%d", &amount);
 	aux = amount;
 	kids list[amount];
@@ -34,7 +33,7 @@ int main (){
 	}
 	
 	printf("Se comportaram: %d | Nao se comportaram: %d\n", good_behavior, bad_behavior);
-	
+	i = 0;
 }
 
 void Order(kids arr[], int size){
@@ -44,7 +43,6 @@ void Order(kids arr[], int size){
 	while(i < size){
 		j = i - 1;
 		aux = arr[i];
-		printf("!%d!", j);
 
 		while (strcmp(arr[j].name, aux.name) > 0 && j >= 0){
 			arr[j + 1] = arr[j];
