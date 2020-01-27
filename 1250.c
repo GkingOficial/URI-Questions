25575015447ed7f3a84601014303b2d655c29647
GkingOficial <59315570+GkingOficial@users.noreply.github.com>
2020-01-27 12:39:47 -0300

    KiloMan.
    Done.

--- 1250.c
+++ 1250.c
@@ -1,6 +1,10 @@
 /*
 Entrada
-A entrada contém vários casos de teste. A primeira linha da entrada contém um inteiro N que indica o número de casos de teste. Cada caso de teste é composto por 3 linhas. A primeira linha contém um inteiro T (1 = T = 50) que indica o número de tiros. A segunda linha contém T inteiros, que representam a sequência das alturas às quais os tiros estão sendo disparados. Cada elemento da sequência será entre 1 e 7, inclusive. A terceira linha da entrada contém a string "pulos", que representa a sequência de pulos que KiloMan tentará; 'J' significa que ele irá pular e 'S' significa que ele ficará parado. Por exemplo, se o primeiro número da sequência de tiros for 3 e o primeiro caractere de "pulos" for 'J', então KiloMan pulará assim que o chefão atirar a uma altura 3 (e, portanto, ele será atingido).
+A entrada contém vários casos de teste. A primeira linha da entrada contém um inteiro N que indica o número de casos de teste. Cada caso de teste é composto por 3
+linhas. A primeira linha contém um inteiro T (1 = T = 50) que indica o número de tiros. A segunda linha contém T inteiros, que representam a sequência das alturas às
+quais os tiros estão sendo disparados. Cada elemento da sequência será entre 1 e 7, inclusive. A terceira linha da entrada contém a string "pulos", que representa a
+sequência de pulos que KiloMan tentará; 'J' significa que ele irá pular e 'S' significa que ele ficará parado. Por exemplo, se o primeiro número da sequência de tiros
+for 3 e o primeiro caractere de "pulos" for 'J', então KiloMan pulará assim que o chefão atirar a uma altura 3 (e, portanto, ele será atingido).
 
 Saída
 Para cada caso, seu programa deve imprimir um inteiro representando o número de vezes que KiloMan é atingido.
