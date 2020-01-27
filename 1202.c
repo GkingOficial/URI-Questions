6e5aaa661180f2db618e5b6fcce3505478a81895
GkingOficial <59315570+GkingOficial@users.noreply.github.com>
2020-01-27 12:09:45 -0300

    Festival das Noites Brancas.
    Done.

--- 1202.c
+++ 1202.c
@@ -1,9 +1,25 @@
-// Inclusão das bibliotecas'stdio', 'string' e 'math'.
+/*
+Entrada
+
+A entrada é composta por diversas instâncias. A primeira linha da entrada contém um inteiro T indicando o número de instâncias.
+Cada instância consiste em uma linha contendo uma descrição de fileira com até 10000 dígitos. A descrição de uma fileira é uma sequência de ’1’s e ’0’s, nunca
+começando com ’0’ (a primeira cadeira de todas as fileiras estão reservadas).
+
+Saída
+Para cada instância imprima os 3 dígitos que devem estar escrito no cartão para a pessoa entrar no teatro.
+*/
+
+
+/* Para este problema, além da utilização de bibliotecas (padrão na maioria dos projetos na linguagem C), utilizei os conceitos de variáveis, vetores, laços de
+repetição, e estruturas condicionais, além da implementação de funções, e, recursividade.*/
+
+// Inclusão das bibliotecas 'stdio', 'string' e 'math'.
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
 
-// Protótipo das funções Recursion: responsável pelo cálculo do n-ésimo elemento da sequência dada; e Bin_To_Dec: responsável pela conversão de um número em binário para decimal.
+/* Protótipo das funções Recursion: responsável pelo cálculo do n-ésimo elemento da sequência dada; e Bin_To_Dec: responsável pela conversão de um número em binário
+para decimal.*/
 int Recursion(int index);
 int Bin_To_Dec(char string[]);
 
@@ -22,7 +38,7 @@ int main(){
 	for(i = 0; i < row; i++){
 		// Leitura de 'in'.
 		scanf("%s", in);
-		// Impressão dos três dígitos que devem estar escrito no cartão para a pessoa entrar no teatro, com 0 ocupando os valores vazios à esquerda.
+		// Impressão dos trás dígitos que devem estar escrito no cartão para a pessoa entrar no teatro, com 0 ocupando os valores vazios à esquerda.
 		printf("%03d\n", Recursion(Bin_To_Dec(in)));
 	}
 }
@@ -44,11 +60,12 @@ int Bin_To_Dec(char string[]){
 	// i e j = contadores; decimal = receberá o valor convertido;
 	int i, j = 0, decimal = 0;
 	
-	// Laço de repetição, executará tantas vezes quanto o número de caracteres de 'string'. "Varrerá" 'string' da direita para a esquerda, ou seja, do final para o ínicio.
+	/* Laço de repetição, executará tantas vezes quanto o número de caracteres de 'string'. "Varrerá" 'string' da direita para a esquerda, ou seja, do final para o
+	ínicio.*/
 	for(i = strlen(string) - 1; i >= 0; i--){
 		// Caso o caracter lido de 'string' seja diferente de 0, então...
 		if(string[i] != '0'){
-			// 'decimal' receberá 2 elevado à j (j é um contador crescente, ou seja, "andará" no sentido oposto à i.
+			// 'decimal' receberá 2 elevado é j (j é um contador crescente, ou seja, "andará" no sentido oposto à i.
 			decimal += pow(2, j);	
 		}
 		// Incremento j, para a próxima leitura.
@@ -57,6 +74,3 @@ int Bin_To_Dec(char string[]){
 	// Retorno decimal.
 	return decimal;
 }
-
-/* Para este problema, além da utilização de bibliotecas (padrão na maioria dos projetos na linguagem C), utilizei os conceitos de variáveis, vetores, laços de repetição, e estruturas condicionais,
-além da implementação de funções, e, recursividade.*/
