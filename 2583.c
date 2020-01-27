f2fed74d7c87b9b3a09c593a199d059d43db5753
GkingOficial <59315570+GkingOficial@users.noreply.github.com>
2020-01-27 13:23:55 -0300

    Chirrin Chirrion
    Done.

--- 2583.c
+++ 2583.c
@@ -1,36 +1,73 @@
+/*
+Entrada
+O primeiro valor a ser lido é um inteiro C, indicando o número de casos de teste. Cada caso de teste inicia com um inteiro N, informando quantas utilizações foram
+feitas. Considere que antes ele não possuía nada, que um Chirrion só terá efeito se ele possuir tal coisa dita, e que um Chirrin só terá efeito se ele ainda não
+possuir tal coisa, ou seja, não tem como ele possuir dois exemplares de uma mesma coisa.
+
+Saída
+Para cada caso de teste, imprima a palavra TOTAL, seguida da relação de coisas que Tausfo tem, em ordem alfabética.
+*/
+
+
+/* Para este problema, além da utilização de bibliotecas (padrão na maioria dos projetos na linguagem C), utilizei os conceitos de variáveis, vetores, laços de
+repetição, e estruturas condicionais, funções, estrutura heterogênea de dados, e, uma função para ordenar um vetor em ordem alfabética juntamente com uma função que
+busca um determinado elemento em um vetor.*/
+
+// Inclusão das bibliotecas'stdio' e 'string'.
 #include <stdio.h>
 #include <string.h>
 
+// Definição de um tipo 'string' (linha 23), sendo este um 'struct' (estrutura de dados heterogêneas).
 typedef struct{
+	// item = armazenará os objetos pedidos.
 	char item[150];
 } string;
 
+// Protótipo das funções Find: responsável por verificar se um elemento pertence à 'string' e Order: responsável pela ordenação de 'string'.
 int Find(string arr[], char str[], int size);
 void Order(string vet[], int size);
 
+// Ínicio da função principal 'main'.
 int main (){
-	// case = casos de teste; uses = vezes que utilizou o Chirrin Chirrion; word = palavra dita na utilização; thing = objeto pedido;
+	// case = casos de teste; uses = vezes que utilizou o Chirrin Chirrion;
 	int cases, uses, aux, i, j, result;
+	// word = palavra dita na utilização; thing = objeto pedido;
 	char word[10], thing[20];
 	
+	// Leitura de cases.
 	scanf("%d", &cases);
+	
+	// Laço de repetição, executará 'cases' vezes.
 	while(cases--){
+		// Leitura de uses.
 		scanf("%d", &uses);
-		
+		// Zero 'i' para utilizar posteriormente em Find (linha).
 		i = 0;
+		// Guardo o valor de 'uses' em 'aux'.
 		aux = uses;
-		string things[aux]; // Crio um vetor --> string --> struct para armazenar os itens pedidos.
+		
+		// Crio um vetor --> string --> struct para armazenar os itens pedidos.
+		string things[aux];
+		
+		// Laço de repetição, executará 'uses' vezes.
 		while(uses--){
-			scanf(" %s %s", thing, word); // Recebo o que foi pedido e a palavra dita na utilização;
-
+			// Recebo o que foi pedido e a palavra dita na utilização;
+			scanf(" %s %s", thing, word);
+			
+			// Se a palavra digitada foi "chirrin", então...
 			if (!strcmp(word, "chirrin")){	
-				// Se a palavra foi "chirrin", chama a função Find que verifica se o objeto pedido já existe na struct. Se não existir, insere no ínicio da struct;
+				// chama a função Find que verifica se o objeto pedido já existe na struct.
 				if (Find(things, thing, i) == -1){
+					// Se não existir, insere no ínicio da struct;
 					strcpy(things[i++].item, thing);
 				}
-			} else if (!strcmp(word, "chirrion")) {// Se a palavra foi "chirrion", chama a função Find que verifica se o objeto pedido já existe na struct. Se existir, insere na struct uma string qualquer;
+				
+			// Se a palavra digitada não foi "chirrin", logo, só poderá ter sido chirrion", então...
+			} else if (!strcmp(word, "chirrion")) {
+				// chama a função Find que verifica se o objeto pedido já existe na struct, caso exista, 'resul' receberá -1.
 				result = Find(things, thing, i);
 				if(result != -1){
+					// Se resul for difere de -1, significa que a palavra digitada pertence à 'string' e insere na struct uma string qualquer;
 					strcpy(things[result].item, "-----------------");
 				}
 			}
@@ -38,20 +75,24 @@ int main (){
 			// Limpando a struct na posição do item, para a proxima leitura;
 			memset(thing, 0, sizeof(thing));
 		}
-
-		Order(things, i); // Chama a função Order, que ordena os itens por ordem alfabética;
-
+		// Chama a função Order, que ordena os itens por ordem alfabética;
+		Order(things, i); 
+		
 		printf("TOTAL\n");
-		// "Printo" os elementos do struct, pulando a posição que tiver a string qualquer ("-----------------", adicionada na linha 34);
+		// Laço que varrerá 'things'
 		for (j = 0; j < i; j++){
+			// Verifico se o item de 'thing' não é a string qualquer ("-----------------", adicionada na linha 71).
 			if (strcmp(things[j].item, "-----------------")){
+				// "Printo" os elementos do struct.
 				printf("%s\n", things[j].item);
 			}
 		}	
 	}
 }
 
+// Funçaõ Find. arr = vetor de 'string'; str = elemento a ser verificado; size = quantidade de elementos lidos.
 int Find(string arr[], char str[], int size){
+	// i = contador;
 	int i;
 	
 	//Verifica se o item já existe no struct, e retorna 1, caso contrário, retorna -1. Basicamene, altera o conteúdo contido na posição i;
@@ -63,12 +104,18 @@ int Find(string arr[], char str[], int size){
 	return -1;	
 }
 
+// Funçaõ Order. arr = vetor do tipo 'string'; size = quantidade de elementos de 'string'.
 void Order(string arr[], int amount){
+	// i e j = contadores; position = guarda a posição de um determinado elemento de 'string';
 	int i, j, position;
+	
+	// aux = variável auxiliar, do tipo 'string'.
 	string aux;
-
+	
+	// Laço de repetição. "Varrerá" 'string'.
 	for (i = 0; i < amount - 1; i++){
-		position = i; // Salva a posição do menor elemento em relação ao i;
+		// Salva a posição do menor elemento em relação ao i;
+		position = i;
 		
 		// Verifica os elementos por ordem alfabetica.
 		for (j = i + 1; j < amount; j++){
@@ -77,7 +124,7 @@ void Order(string arr[], int amount){
 			}
 		}
 		
-		// Organiza os elementos de acordo com a verificação realizada pelo 'for' anterior.
+		// Organiza os elementos de acordo com a verificação realizada pelo 'for' anterior (linha 121).
 		aux = arr[position];
 		arr[position] = arr[i];
 		arr[i] = aux;
