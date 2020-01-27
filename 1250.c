1dab518d682a0e2a6e6c5b15ead2e2ca5e515d90
GkingOficial <59315570+GkingOficial@users.noreply.github.com>
2020-01-27 12:22:56 -0300

    KiloMan.
    Done.

--- 1250.c
+++ 1250.c
@@ -1,9 +1,22 @@
+/*
+Entrada
+A entrada contém vários casos de teste. A primeira linha da entrada contém um inteiro N que indica o número de casos de teste. Cada caso de teste é composto por 3 linhas. A primeira linha contém um inteiro T (1 = T = 50) que indica o número de tiros. A segunda linha contém T inteiros, que representam a sequência das alturas às quais os tiros estão sendo disparados. Cada elemento da sequência será entre 1 e 7, inclusive. A terceira linha da entrada contém a string "pulos", que representa a sequência de pulos que KiloMan tentará; 'J' significa que ele irá pular e 'S' significa que ele ficará parado. Por exemplo, se o primeiro número da sequência de tiros for 3 e o primeiro caractere de "pulos" for 'J', então KiloMan pulará assim que o chefão atirar a uma altura 3 (e, portanto, ele será atingido).
+
+Saída
+Para cada caso, seu programa deve imprimir um inteiro representando o número de vezes que KiloMan é atingido.
+*/
+
+
+/* Para este problema, além da utilização de bibliotecas (padrão na maioria dos projetos na linguagem C), utilizei os conceitos de variáveis, vetores, laços de
+repetição, e estruturas condicionais. */
+
 // Inclusão da biblioteca 'stdio' para poder utilizar as funções 'printf' e 'scanf'
 #include <stdio.h> 
 
 // Ínicio da função principal 'main'.
 int main(){
-	// cases = quantidade de casos a serem testados; shots = quantidade de tiros disprados; hitted = aramazenará quantas vezes KiloMan foi atingido; array_shots = armazenará a altura dos tiros;
+	/* cases = quantidade de casos a serem testados; shots = quantidade de tiros disprados; hitted = aramazenará quantas vezes KiloMan foi atingido;
+	array_shots = armazenará a altura dos tiros;*/
 	int cases, shots, i, j, hitted = 0, array_shots[51]; 
 	
 	// array_states = armazenará as ações (S - Stop; J - Jump) de KiloMan;
@@ -52,6 +65,3 @@ int main(){
 		hitted = 0;
 	}
 }
-
-
-/* Para este problema, além da utilização de bibliotecas (padrão na maioria dos projetos na linguagem C), utilizei os conceitos de variáveis, vetores, laços de repetição, e estruturas condicionais. */
